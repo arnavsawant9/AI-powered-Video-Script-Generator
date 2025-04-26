f8e09b95a17d2ac2b0531caa85a7ad8ce737e1ae
arnavsawant9 <arnav.sawantbhosale@gmail.com>
2025-04-26 21:37:54 +0530

    done final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://ai-powered-video-script-generator.onrender.com/api';
 
 const apiService = {
   // Generate a script
